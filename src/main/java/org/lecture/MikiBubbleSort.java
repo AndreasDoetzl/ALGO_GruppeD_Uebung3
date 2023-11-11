c855401e936baaa0045a25a605584e73fe4cd49c
Mikos Komlosy <2220859001@fh-burgenland.at>
2023-11-11 11:35:58 +0100

    Add neu Klass MikiBubblesort

---
+++ src/main/java/org/lecture/MikiBubbleSort.java
@@ -0,0 +1,24 @@
+package org.lecture;
+
+public class MikiBubbleSort implements CustomArraySorter {
+    /**
+     * Eine Implementierung des BubbleSort-Algorithmus für die CustomArraySorter-Schnittstelle.
+     */
+    @Override
+    public void sort(CustomArray array) {
+        int n = array.length();
+
+        for (int k=0; k < n-1; k++) {
+            for (int i = 0; i < n - 1; i++) {
+                Integer testedNumber = array.getValue(i);
+                Integer nextNumber = array.getValue(i + 1);
+                if (testedNumber > nextNumber) {
+                    array.setValue(i, nextNumber);
+                    array.setValue((i + 1), testedNumber);
+                }
+            }
+            n -= 1;
+        }
+        System.out.println("\nBubble sort is ready");
+    }
+}
